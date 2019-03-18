ab9f08c029611611dd2a94d09731e0861c0e0369
ANDERSON S VIEIRA <anderson.vieira-ext@viavarejo.com.br>
2019-03-18 12:50:20 -0300

    Implementação de tela de recebimento de mensagem

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Chat App",
       debugShowCheckedModeBanner: false,
-      theme: Theme.of(context).platform == TargetPlatform.iOS
+      theme: Theme
+          .of(context)
+          .platform == TargetPlatform.iOS
           ? kIOSTheme
           : kDefaultTheme,
       home: ChatScreen(),
@@ -43,10 +45,24 @@ class _ChatScreenState extends State<ChatScreen> {
           title: Text("Chat App"),
           centerTitle: true,
           elevation:
-              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+          Theme
+              .of(context)
+              .platform == TargetPlatform.iOS ? 0.0 : 4.0,
         ),
         body: Column(
           children: <Widget>[
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ChatMessage(),
+                  ChatMessage(),
+                  ChatMessage()
+                ],
+              ),
+            ),
+            Divider(
+              height: 1,
+            ),
             Container(
               decoration: BoxDecoration(
                 color: Theme.of(context).cardColor,
@@ -71,23 +87,27 @@ class _TextComposerState extends State<TextComposer> {
   @override
   Widget build(BuildContext context) {
     return IconTheme(
-      data: IconThemeData(color: Theme.of(context).accentColor),
+      data: IconThemeData(color: Theme
+          .of(context)
+          .accentColor),
       child: Container(
         margin: EdgeInsets.symmetric(horizontal: 8),
-        decoration: Theme.of(context).platform == TargetPlatform.iOS
+        decoration: Theme
+            .of(context)
+            .platform == TargetPlatform.iOS
             ? BoxDecoration(
-                border: Border(top: BorderSide(color: Colors.grey[200])))
+            border: Border(top: BorderSide(color: Colors.grey[200])))
             : null,
         child: Row(
           children: <Widget>[
             Container(
               child:
-                  IconButton(icon: Icon(Icons.photo_camera), onPressed: () {}),
+              IconButton(icon: Icon(Icons.photo_camera), onPressed: () {}),
             ),
             Expanded(
               child: TextField(
                 decoration:
-                    InputDecoration.collapsed(hintText: "Enviar mensagem"),
+                InputDecoration.collapsed(hintText: "Enviar mensagem"),
                 onChanged: (text) {
                   setState(() {
                     _isComposing = text.length > 0;
@@ -97,15 +117,17 @@ class _TextComposerState extends State<TextComposer> {
             ),
             Container(
                 margin: const EdgeInsets.symmetric(horizontal: 4),
-                child: Theme.of(context).platform == TargetPlatform.iOS
+                child: Theme
+                    .of(context)
+                    .platform == TargetPlatform.iOS
                     ? CupertinoButton(
-                        child: Text("Enviar"),
-                        onPressed: _isComposing ? () {} : null,
-                      )
+                  child: Text("Enviar"),
+                  onPressed: _isComposing ? () {} : null,
+                )
                     : IconButton(
-                        icon: Icon(Icons.send),
-                        onPressed: _isComposing ? () {} : null,
-                      ))
+                  icon: Icon(Icons.send),
+                  onPressed: _isComposing ? () {} : null,
+                ))
           ],
         ),
       ),
@@ -119,11 +141,29 @@ class ChatMessage extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
             margin: const EdgeInsets.only(right: 10),
             child: CircleAvatar(
-              backgroundImage: NetworkImage("https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiatcDWg4zhAhV7GLkGHcyMCmQQjRx6BAgBEAU&url=https%3A%2F%2Ficonscout.com%2Ficon%2Favatar-372&psig=AOvVaw1HXw8m6xrh4_t93tSp-EB5&ust=1553010116920802") ,
+              backgroundImage: NetworkImage(
+                  "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiatcDWg4zhAhV7GLkGHcyMCmQQjRx6BAgBEAU&url=https%3A%2F%2Ficonscout.com%2Ficon%2Favatar-372&psig=AOvVaw1HXw8m6xrh4_t93tSp-EB5&ust=1553010116920802"),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text("Anderson",
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .subhead,),
+                Container(
+                  margin: const EdgeInsets.only(top: 5),
+                  child: Text("Texto"),
+                )
+              ],
             ),
           )
         ],
